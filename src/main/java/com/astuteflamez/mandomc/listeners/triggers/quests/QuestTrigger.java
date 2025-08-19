ac677f8adf5d50af4132bf309a69c2c116c154f0
BatmenzDW <turtleterracotta@gmail.com>
2025-08-19 18:07:30 -0400

    Added QuestTrigger abstract

---
+++ src/main/java/com/astuteflamez/mandomc/listeners/triggers/quests/QuestTrigger.java
@@ -0,0 +1,39 @@
+package com.astuteflamez.mandomc.listeners.triggers.quests;
+
+import com.astuteflamez.mandomc.database.PlayerQuestsTable;
+import com.astuteflamez.mandomc.database.data.PlayerQuest;
+import org.bukkit.Bukkit;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class QuestTrigger {
+    protected String trigger;
+
+    protected List<PlayerQuest> getTriggeredQuests(Player player){
+        ConsoleCommandSender console = Bukkit.getConsoleSender();
+        List<PlayerQuest> quests = new ArrayList<PlayerQuest>();
+
+        try {
+            quests = PlayerQuestsTable.getTriggeredQuests(player.getUniqueId().toString(), trigger);
+        } catch (SQLException e) {
+            console.sendMessage("[MandoMC] there was an issue triggering " + trigger + " for "  + player.getName());
+        }
+
+        return quests;
+    }
+
+    protected void updateTriggeredQuests(Player player, List<PlayerQuest> quests){
+        ConsoleCommandSender console = Bukkit.getConsoleSender();
+        try {
+            for (PlayerQuest quest : quests) {
+                PlayerQuestsTable.updateQuestProgress(player.getUniqueId().toString(), quest.getQuestName(), quest.getQuestProgress());
+            }
+        } catch (SQLException e) {
+            console.sendMessage("[MandoMC] there was an issue updating quests triggered by " + trigger + " for "  + player.getName());
+        }
+    }
+}
