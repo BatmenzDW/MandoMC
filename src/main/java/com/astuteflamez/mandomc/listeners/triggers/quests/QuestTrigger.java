eace972b84497df956f0394cfc2daebee5647edd
BatmenzDW <turtleterracotta@gmail.com>
2025-08-22 16:26:58 -0400

    Started working on Advancements

--- src/main/java/com/astuteflamez/mandomc/listeners/triggers/quests/QuestTrigger.java
+++ src/main/java/com/astuteflamez/mandomc/listeners/triggers/quests/QuestTrigger.java
@@ -1,9 +1,12 @@
 package com.astuteflamez.mandomc.listeners.triggers.quests;
 
 import com.astuteflamez.mandomc.LangConfig;
+import com.astuteflamez.mandomc.MandoMC;
 import com.astuteflamez.mandomc.database.PlayerQuestsTable;
 import com.astuteflamez.mandomc.database.data.PlayerQuest;
 import org.bukkit.Bukkit;
+import org.bukkit.NamespacedKey;
+import org.bukkit.advancement.AdvancementProgress;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
@@ -15,7 +18,7 @@ import java.util.List;
 
 public abstract class QuestTrigger implements Listener {
 
-    protected List<PlayerQuest> getTriggeredQuests(Player player, String trigger){
+    protected static List<PlayerQuest> getTriggeredQuests(Player player, String trigger){
         ConsoleCommandSender console = Bukkit.getConsoleSender();
         List<PlayerQuest> quests = new ArrayList<PlayerQuest>();
 
@@ -28,7 +31,7 @@ public abstract class QuestTrigger implements Listener {
         return quests;
     }
 
-    protected void updateTriggeredQuests(Player player, List<PlayerQuest> quests, String trigger){
+    protected static void updateTriggeredQuests(Player player, List<PlayerQuest> quests, String trigger){
         ConsoleCommandSender console = Bukkit.getConsoleSender();
         try {
             for (PlayerQuest quest : quests) {
@@ -39,7 +42,7 @@ public abstract class QuestTrigger implements Listener {
         }
     }
 
-    protected void triggerQuests(Player player, String trigger, float amount){
+    protected static void triggerQuests(Player player, String trigger, float amount){
         FileConfiguration config = LangConfig.get();
         List<PlayerQuest> quests = getTriggeredQuests(player, trigger);
 
@@ -47,8 +50,26 @@ public abstract class QuestTrigger implements Listener {
         for (PlayerQuest quest : quests){
             quest.setQuestProgress(quest.getQuestProgress() + amount);
             player.sendMessage(String.format(config.getString("quests.progress", "InvalidKey"), quest.getQuestName(), quest.getQuestProgress() * 100));
+            if (quest.getQuestProgress() >= 1.0f){
+                triggerAdvancement(player, quest.getQuestName());
+            }
         }
 
         updateTriggeredQuests(player, quests, trigger);
+    }
+
+    public static void triggerAdvancement(Player player, String quest){
+        FileConfiguration config = LangConfig.get();
+
+        String questKey = config.getString("quests." + quest + ".advancement");
+        if (questKey == null) return;
+
+        NamespacedKey advKey = NamespacedKey.fromString(questKey, MandoMC.getInstance());
+
+        AdvancementProgress progress = player.getAdvancementProgress(Bukkit.getAdvancement(advKey));
+        for (String criteria : progress.getRemainingCriteria())
+        {
+            progress.awardCriteria(criteria);
+        }
     }
 }
